43dffcb81116727cb77de7c498a4fc1079d2c034
SouthViking <seb.toro.severino@gmail.com>
2023-07-23 21:33:37 -0400

    feat: graphene base installation

--- core/settings.py
+++ core/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'users'
+    'users',
+    'graphene_django',
 ]
 
 MIDDLEWARE = [
@@ -128,3 +129,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Graphene settings
+
+GRAPHENE = {
+    'SCHEMA': 'core.schema.schema',
+}
--- core/urls.py
+++ core/urls.py
@@ -14,9 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
+from django.contrib import admin
+from django.conf import settings
+from graphene_django.views import GraphQLView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('graphql', GraphQLView.as_view(graphiql = settings.DEBUG or False)),
 ]

dbb27173f4b06e1f1b8e07a5587fb61354a0fba3
SouthViking <seb.toro.severino@gmail.com>
2023-07-26 17:12:45 -0400

    docs: updating tryable_mutation decorator docs

--- core/decorators.py
+++ core/decorators.py
@@ -4,8 +4,13 @@ from django.core.exceptions import ValidationError
 from .utils import get_empty_entries, camelize_list
 
 def tryable_mutation(required_fields: Union[List[str], None] = None):
+    """
+    Adds a try/except wrapper to the target function and returns error messages for some common exceptions.
+
+    Parameters:
+        required_fields (List[str]): An optional list containing the required fields. Will return an error response in case any of them are missing.
+    """
     def decorator(func):
-        """Adds a try/except wrapper to the target function and returns error messages for some common exceptions."""
         def wrapper(*args, **kwargs):
             try:
                 empty_keys = get_empty_entries(kwargs, required_fields)
@@ -19,7 +24,6 @@ def tryable_mutation(required_fields: Union[List[str], None] = None):
                 return func(*args, **kwargs)
 
             except ValidationError as error:
-                print(error.error_dict)
                 return {
                     'success': False,
                     'message': 'There has been a validation error while trying to register the user. Check the required fields and try again.',
@@ -27,7 +31,6 @@ def tryable_mutation(required_fields: Union[List[str], None] = None):
                 }
 
             except Exception as error:
-                print(error)
                 return {
                     'success': False,
                     'message': 'There has been an internal error. Please try again later.',

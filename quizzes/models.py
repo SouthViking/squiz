c0e064371d7d384e85dddc44337153f3a0154abf
SouthViking <seb.toro.severino@gmail.com>
2023-07-26 16:08:31 -0400

    feat: creating quizzes models

--- quizzes/models.py
+++ quizzes/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+from users.models import User
+
+class Quiz(models.Model):
+    title = models.CharField(max_length = 50)
+    summary = models.TextField()
+    max_solving_time_mins = models.IntegerField()
+    is_public = models.BooleanField(default = False)
+    is_scheduled = models.BooleanField(default = False)
+    starts_at = models.DateTimeField()
+    deadline = models.DateTimeField()
+    created_at = models.DateTimeField()
+    creator = models.ForeignKey(User, on_delete = models.CASCADE)
+
+    class Meta:
+        db_table = 'quiz'
+
+class Question(models.Model):
+    title = models.CharField(max_length = 250)
+    description = models.TextField()
+    score = models.IntegerField(default = 0)
+    quiz = models.ForeignKey(Quiz, on_delete = models.CASCADE)
+
+    class Meta:
+        db_table = 'question'
+
+class Option(models.Model):
+    label = models.CharField(max_length = 100)
+    is_correct = models.BooleanField(default = False)
+    feedback_message = models.CharField(max_length = 250)
+    question = models.ForeignKey(Question, on_delete = models.CASCADE)
+
+    class Meta:
+        db_table = 'quiz_option'

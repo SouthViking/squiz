6fa51fb480f337c516fd9848d0532822b5bd5a3f
SouthViking <seb.toro.severino@gmail.com>
2023-07-28 12:54:12 -0400

    fix: fixing datetime diff in quiz interval validation

--- quizzes/utils.py
+++ quizzes/utils.py
@@ -12,7 +12,7 @@ def is_quiz_time_interval_valid(starts_at: datetime, ends_at: datetime, max_solv
         return (False, 'End time must be greater than the start time.')
     
     max_solving_secs = max_solving_mins * 60
-    full_solving_window_secs = (ends_at - starts_at).seconds
+    full_solving_window_secs = (ends_at - starts_at).total_seconds()
 
     if max_solving_secs > full_solving_window_secs:
         return (False, f'Solving time is not valid. The maximum amount of minutes for the current interval is {int(full_solving_window_secs/60)} minutes ({max_solving_mins} specified).')

1e040231929a5a136e4cdbb317c57c08fdbf4d3c
SouthViking <seb.toro.severino@gmail.com>
2023-08-03 15:53:29 -0400

    feat: removing unverified accounts in job

--- users/jobs.py
+++ users/jobs.py
@@ -9,7 +9,7 @@ from scheduler.types import IntervalJobDefinition
 
 def remove_expired_unverified_accounts():
     logger.info('Executing process to remove unverified accounts.')
-    target_accounts = User.objects.filter(is_verified = False).values('email', 'created_at')
+    target_accounts = User.objects.filter(is_verified = False).values('id', 'email', 'created_at')
     logger.info(f'Found {len(target_accounts)} unverified account(s).')
 
     for account in target_accounts:
@@ -21,14 +21,15 @@ def remove_expired_unverified_accounts():
             continue
 
         logger.info(f'Removing account for email: {account["email"]}. ({hours_diff} hours have elapsed since registration)')
+        user_record: User = User.objects.get(id = account["id"])
+        user_record.delete()
 
-        # TODO: Deletion of all data for the current account.
-
+        logger.info(f'Account with email {account["email"]} has been removed correctly.')
 
 interval_jobs: List[IntervalJobDefinition] = [
     {
         'func': remove_expired_unverified_accounts,
         'id': 'remove_expired_unverified_accounts',
-        'seconds': 5,
+        'seconds': 1800,
     }
 ]
